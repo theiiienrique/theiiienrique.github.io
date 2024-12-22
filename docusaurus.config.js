cac02fd8247342ecc18f8bbd03b014557a8aef8f
Enrique Martinez III <82408876+theiiienrique@users.noreply.github.com>
2024-12-21 20:11:19 -0800

    add contact form

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,109 +4,104 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
-import remarkDefList from 'remark-deflist';
-require('dotenv').config();
+import { themes as prismThemes } from "prism-react-renderer";
+import remarkDefList from "remark-deflist";
+require("dotenv").config();
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Enrique Martinez III',
-  tagline: 'Technical Writer',
-  favicon: 'img/favicon.ico',
-  staticDirectories: ['public', 'static'],
+  title: "Enrique Martinez III",
+  tagline: "Technical Writer",
+  favicon: "img/favicon.ico",
+  staticDirectories: ["public", "static"],
 
   // Set the production url of your site here
-  url: 'https://theiiienrique.github.io',
+  url: "https://theiiienrique.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'theiiienrique', // Usually your GitHub org/user name.
-  projectName: 'theiiienrique.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "theiiienrique", // Usually your GitHub org/user name.
+  projectName: "theiiienrique.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           showLastUpdateTime: true,
           breadcrumbs: true,
-          remarkPlugins: [
-            remarkDefList,
-          ],
+          remarkPlugins: [remarkDefList],
         },
         pages: {
-          remarkPlugins: [
-            remarkDefList,
-          ],
+          remarkPlugins: [remarkDefList],
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
         gtag: {
-          trackingID: 'G-2WFMMZTEFB',
+          trackingID: "G-2WFMMZTEFB",
           anonymizeIP: true,
         },
         googleTagManager: {
-          containerId: 'GTM-T5D7VXBZ',
+          containerId: "GTM-T5D7VXBZ",
         },
       }),
     ],
   ],
 
-  plugins: [
-    require.resolve('docusaurus-plugin-image-zoom'),
-  ],
+  plugins: [require.resolve("docusaurus-plugin-image-zoom")],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       algolia: {
-        appId: '749TA7V8JC',
-        apiKey: '228b1afe8e2f2f99875d6a9c4471252d',
-        indexName: 'theiiienriqueio',
+        appId: "749TA7V8JC",
+        apiKey: "228b1afe8e2f2f99875d6a9c4471252d",
+        indexName: "theiiienriqueio",
       },
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         // disableSwitch: true,
       },
       navbar: {
-        title: 'Enrique Martinez III',
+        title: "Enrique Martinez III",
         items: [
-          {to: '/about', label: 'About', position: 'left'},
+          { to: "/about", label: "About", position: "left" },
           {
-            type: 'docSidebar',
-            sidebarId: 'docsSidebar',
-            position: 'left',
-            label: 'Docs',
+            type: "docSidebar",
+            sidebarId: "docsSidebar",
+            position: "left",
+            label: "Docs",
+          },
+          { to: "/contact", label: "Contact", position: "left" },
+          {
+            href: "https://www.linkedin.com/in/theiiienrique/",
+            position: "right",
+            className: "navbar--linkedin-link",
+            "aria-label": "LinkedIn profile",
           },
           {
-            href: 'https://www.linkedin.com/in/theiiienrique/',
-            position: 'right',
-            className: 'navbar--linkedin-link',
-            'aria-label': 'LinkedIn profile',
-          },
-          {
-            'href': 'https://github.com/theiiienrique',
-            'position': 'right',
-            'className': 'navbar--github-link',
-            'aria-label': 'GitHub profile',
+            href: "https://github.com/theiiienrique",
+            position: "right",
+            className: "navbar--github-link",
+            "aria-label": "GitHub profile",
           },
         ],
       },
@@ -124,10 +119,10 @@ const config = {
         darkTheme: prismThemes.oceanicNext,
       },
       zoom: {
-        selector: '.markdown :not(em) > img',
+        selector: ".markdown :not(em) > img",
         background: {
-          light: 'rgba(0, 0, 0, 0.75)',
-          dark: 'rgba(0, 0, 0, 0.75)',
+          light: "rgba(0, 0, 0, 0.75)",
+          dark: "rgba(0, 0, 0, 0.75)",
         },
       },
     }),
