eca612302d5abc08aeb5b9c4055903baeee27402
Enrique Martinez III <82408876+theiiienrique@users.noreply.github.com>
2026-01-03 10:34:08 -0800

    Update link to point to WordPress documentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/vale"
+            to="/docs/wordpress"
           >
             View my work
           </Link>
